7fe917a24569c385141ab1d8812ef817b9fb8a82
ethan grossman <ethangrossman3@gmail.com>
2023-03-22 00:14:29 -0500

    finished exercise 2

---
+++ Palindrome_Exercise.NET6/Palindrome_Exercise.NET6.Tests/WordSmithTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome_Exercise.NET6.Tests
+{
+    public class WordSmithTest
+    {
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("Racecar", true)]
+        [InlineData("hannah", true)]
+        [InlineData("kayak", true)]
+        [InlineData("John", false)]
+        [InlineData("Jose", false)]
+        [InlineData("Amoriss", false)]
+        [InlineData("", false)]
+        [InlineData(null, false)]
+        public void IsAPalindrome_Test(string word, bool expected)
+        {
+            //arrange
+            var ws = new WordSmith();
+
+            //act
+            var actual = ws.IsAPalindrome(word);    
+
+            //assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
